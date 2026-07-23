3aeedfb4d75c23d576a572645112d545da0603f3
unknown <gss.devtech@gmail.com>
2026-07-23 13:58:28 -0300

    Consulta com vários critérios

--- src/main/java/com/GabrielSales/workshopmongo/services/PostServices.java
+++ src/main/java/com/GabrielSales/workshopmongo/services/PostServices.java
@@ -6,6 +6,7 @@ import com.GabrielSales.workshopmongo.services.exception.ObjectNotFoundException
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,10 @@ public class PostServices {
     public List<Post> findByTitle(String text){
         // return repo.findByTitleContainingIgnoreCase(text); abaixo segue metodo alternativo
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + (24 * 60 * 60 * 1000));
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
